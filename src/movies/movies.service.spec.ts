ef422286fe4f9adb3457ee42950ef64d9af40f91
pki071120 <kollagajoha@gmail.com>
2024-04-18 14:57:34 +0900

    test delete,create,upadate

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -36,13 +36,68 @@ describe('MoviesService', () => {
       expect(movie).toBeDefined();
       expect(movie.id).toEqual(1);
     });
-    it("shlould throw 404 error", () => {
-      try{
+    it('shlould throw 404 error', () => {
+      try {
         service.getOne(999);
-      } catch(e){
+      } catch (e) {
         expect(e).toBeInstanceOf(NotFoundException);
-        expect(e.message).toEqual("Movie with ID 999 not found");
+        expect(e.message).toEqual('Movie with ID 999 not found');
       }
-    })
+    });
+  });
+
+  describe('deleteOne', () => {
+    it('should delete a movie', () => {
+      service.createMovie({
+        title: 'test',
+        geners: ['test'],
+        year: 2020,
+      });
+      const beforeDelete = service.getAll().length;
+      service.deleteOne(1);
+      const afterDelete = service.getAll().length;
+      expect(afterDelete).toBeLessThan(beforeDelete);
+    });
+    it('should return a 404', () => {
+      try {
+        service.deleteOne(999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    });
+  });
+
+  describe('createMovie', () => {
+    it('should create a movie', () => {
+      const beforeCreate = service.getAll().length;
+      service.createMovie({
+        title: 'test',
+        geners: ['test'],
+        year: 2020,
+      });
+      const afterCreate = service.getAll().length;
+      expect(afterCreate).toBeGreaterThan(beforeCreate);
+    });
+  });
+
+  describe('update', () => {
+    it('should update a movie', () => {
+      service.createMovie({
+        title: 'test',
+        geners: ['test'],
+        year: 2020,
+      });
+      service.update(1, { title: 'update' });
+      const movie = service.getOne(1);
+      expect(movie.title).toEqual('update');
+    });
+
+    it('should throw a NotFoundExeception', () => {
+      try {
+        service.update(999, { title: 'update' });
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    });
   });
 });
